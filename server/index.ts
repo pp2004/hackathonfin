fd5b383299f0596e4e962c2cac68420265d349f2
pp2004 <prakash.paarth@gmail.com>
2025-07-08 13:23:49 +0530

    Initial commit of InvestorPro project

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
@@ -59,11 +60,10 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
+  const port = 5009;
   server.listen({
     port,
     host: "0.0.0.0",
-    reusePort: true,
   }, () => {
     log(`serving on port ${port}`);
   });
